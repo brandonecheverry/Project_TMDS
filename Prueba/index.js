e9d3da22782e1e0f518132aa9b8f8b8063aa01d3
Brandon Echeverry <b.echeverry@refacil.com>
2023-02-28 21:04:43 -0500

    Eliminar Prueba

--- Prueba/index.js
+++
@@ -1,4 +0,0 @@
-let project = "juanmi";
-let arreglo = {personaje: "juanmi",edad: 302,extra:'012345'};
-let pruebaProject = arreglo.personaje == project && arreglo.extra ?arreglo.extra.replace(/^0+/, '') : arreglo.serviceNumber;
-console.log(pruebaProject);
